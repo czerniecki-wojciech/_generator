1c18d808915686d3d18b2b92c4e172ee129b8015
Czerniecki Wojciech <czerniecki.wojciech@gmail.com>
2016-01-18 17:29:21 +0100

    fake evolution

--- PermutationGenerator.cpp
+++ PermutationGenerator.cpp
@@ -10,10 +10,16 @@ SimulationData PermutationGenerator::nextPermutation()
     SimulationData sd(simulationData);
     for(uint i=0; i< simulationData.getElementsNum(); ++i)
     {
-        uint availableFin = sd.getTotalElementsCost();
+        uint availableFin = maxCost - sd.getTotalElementsCost();
         sd.setNumOfReplaceKitForElement(i, 1);
-        uint repairKitsNum = rand() % (availableFin / sd.getTotalCostOfElement(i));
-        sd.setNumOfReplaceKitForElement(i, repairKitsNum);
+		if (availableFin > 0 && sd.getTotalCostOfElement(i) < availableFin)
+		{
+			uint repairKitsNum = rand() % (availableFin / sd.getTotalCostOfElement(i));
+			sd.setNumOfReplaceKitForElement(i, repairKitsNum);
+		}
+		else {
+			sd.setNumOfReplaceKitForElement(i, 0);
+		}
     }
 
     return sd;
