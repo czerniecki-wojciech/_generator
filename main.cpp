2d1b8a3d92adf911ccd1b061f3d8da3d2ba3e6d1
Czerniecki Wojciech <czerniecki.wojciech@gmail.com>
2016-01-18 14:01:22 +0100

    print results

--- main.cpp
+++ main.cpp
@@ -54,10 +54,51 @@ int main()
         ++pi;
     }
 
+    uint bestWorkingTime = 0;
+    uint bestTotalTime = 0;
+    float bestPercentOfWorkingTime = 0.0f;
+
+    uint bestWorkingTimeNumOfConservators = 1;
+    uint bestTotalTimeNumOfConservators = 1;
+    uint bestPercentOfWorkingTimeNumOfConservators = 1;
+
+    uint indexBestWorkingTime = 0;
+    uint indexBestTotalTime = 0;
+    uint indexBestPercentOfWorkingTime = 0;
+
     for(uint numOfConservators=1; numOfConservators<=maxNumberOfConservators; ++numOfConservators)
     {
-        bruteforceByNumOfConservators.at(numOfConservators).size();
+        uint size = bruteforceByNumOfConservators.at(numOfConservators-1).size();
+
+        for(uint i = 0; i<size; ++i)
+        {
+            SimulationResult current = bruteforceByNumOfConservators.at(numOfConservators-1).at(i).first;
+
+            if(current.workingTime >= bestWorkingTime)
+            {
+                bestWorkingTime = current.workingTime;
+                indexBestWorkingTime = i;
+                bestWorkingTimeNumOfConservators = numOfConservators;
+            }
+
+            if(current.totalTime >= bestTotalTime)
+            {
+                bestTotalTime = current.totalTime;
+                indexBestTotalTime = i;
+                bestTotalTimeNumOfConservators = numOfConservators;
+            }
+
+            if(current.workingTime / current.totalTime >= bestPercentOfWorkingTime)
+            {
+                bestPercentOfWorkingTime = current.workingTime / current.totalTime;
+                indexBestPercentOfWorkingTime = i;
+                bestPercentOfWorkingTimeNumOfConservators = numOfConservators;
+            }
+        }
     }
+
+    std::cout << "Best working time: " << bruteforceByNumOfConservators.at(bestWorkingTimeNumOfConservators-1).at(indexBestWorkingTime).first.workingTime << std::endl;
+    std::cout << bruteforceByNumOfConservators.at(bestWorkingTimeNumOfConservators-1).at(indexBestWorkingTime).second << std::endl;
 
     return 0;
 }
